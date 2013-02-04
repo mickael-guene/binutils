5d2f5b154c4c2fe0d63b27b359680f81a66b571f
Mickael Guene <mickael.guene@st.com>
2013-02-04 13:36:49 +0100

    Translate R_ARM_TARGET2 relocation into R_ARM_GOT32 relocation for FDPIC platform

--- bfd/elf32-arm.c
+++ bfd/elf32-arm.c
@@ -6960,7 +6960,9 @@ bfd_elf32_arm_set_target_relocs (struct bfd *output_bfd,
     return;
 
   globals->target1_is_rel = target1_is_rel;
-  if (strcmp (target2_type, "rel") == 0)
+  if (globals->fdpic_p)
+    globals->target2_reloc = R_ARM_GOT32;
+  else if (strcmp (target2_type, "rel") == 0)
     globals->target2_reloc = R_ARM_REL32;
   else if (strcmp (target2_type, "abs") == 0)
     globals->target2_reloc = R_ARM_ABS32;
