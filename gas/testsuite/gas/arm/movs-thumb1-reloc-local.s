d31ed17039321315412dbd70b2795f225fe3ff43
Mickael Guene <mickael.guene@st.com>
2015-12-08 08:45:28 +0100

    Update testes against new gas names

--- gas/testsuite/gas/arm/movs-thumb1-reloc-local.s
+++ gas/testsuite/gas/arm/movs-thumb1-reloc-local.s
@@ -3,10 +3,10 @@
 .syntax unified
 .thumb
 foo:
-movs r0, #:high_high:#bar
-movs r0, #:high_low:#bar
-movs r0, #:low_high:#bar
-movs r0, #:low_low:#bar
+movs r0, #:upper8_15:#bar
+movs r0, #:upper0_7:#bar
+movs r0, #:lower8_15:#bar
+movs r0, #:lower0_7:#bar
 
 .space 0x10000
 
--- ld/testsuite/ld-arm/thumb1-movs.s
+++ ld/testsuite/ld-arm/thumb1-movs.s
@@ -7,25 +7,25 @@
 	.type	thumb1, %function
 _start:
 thumb1:
-	movs r0, #:high_high:#thumb3
-	movs r1, #:high_low:#thumb3
-	movs r2, #:low_high:#thumb1
-	movs r3, #:low_low:#thumb1
-	movs r4, #:low_low:#thumb3
-	movs r5, #:low_high:#thumb3
-	movs r6, #:high_low:#thumb1
-	movs r7, #:high_high:#thumb1
+	movs r0, #:upper8_15:#thumb3
+	movs r1, #:upper0_7:#thumb3
+	movs r2, #:lower8_15:#thumb1
+	movs r3, #:lower0_7:#thumb1
+	movs r4, #:lower0_7:#thumb3
+	movs r5, #:lower8_15:#thumb3
+	movs r6, #:upper0_7:#thumb1
+	movs r7, #:upper8_15:#thumb1
 	.thumb_func
 	.type	thumb2, %function
 thumb2:
-	movs r0, #:high_high:#(thumb3 + 0)
-	movs r1, #:high_low:#(thumb2 + 1)
-	movs r2, #:low_high:#(var1 + 255)
-	movs r3, #:low_low:#var1
-	movs r7, #:high_high:#var1 + 4
-	movs r6, #:high_low:#var2
-	movs r5, #:low_high:#var2 + 0xff
-	movs r4, #:low_low:#var2 - (-1)
+	movs r0, #:upper8_15:#(thumb3 + 0)
+	movs r1, #:upper0_7:#(thumb2 + 1)
+	movs r2, #:lower8_15:#(var1 + 255)
+	movs r3, #:lower0_7:#var1
+	movs r7, #:upper8_15:#var1 + 4
+	movs r6, #:upper0_7:#var2
+	movs r5, #:lower8_15:#var2 + 0xff
+	movs r4, #:lower0_7:#var2 - (-1)
 var1:
 	.byte 1
 var2:
@@ -35,8 +35,8 @@ var2:
 	.thumb_func
 	.type	thumb3, %function
 thumb3:
-	movs r0, #:high_high:#thumb1
-	movs r1, #:high_low:#thumb2
-	movs r2, #:low_high:#thumb3
-	movs r3, #:low_low:#thumb1
+	movs r0, #:upper8_15:#thumb1
+	movs r1, #:upper0_7:#thumb2
+	movs r2, #:lower8_15:#thumb3
+	movs r3, #:lower0_7:#thumb1
 
